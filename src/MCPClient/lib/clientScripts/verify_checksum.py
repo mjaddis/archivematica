4f0a6eddb81d3eafbfdb0faefde27c5ace95d81c
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2019-04-22 14:57:09 +0200

    Remove extraneous arguments

--- src/MCPClient/lib/clientScripts/verify_checksum.py
+++ src/MCPClient/lib/clientScripts/verify_checksum.py
@@ -233,7 +233,7 @@ def run_hashsum_commands(job):
     transfer_uuid = None
     try:
         transfer_dir = job.args[1]
-        transfer_uuid = job.args[4]
+        transfer_uuid = job.args[2]
     except IndexError:
         logger.error("Cannot access expected module arguments: %s", job.args)
         return 1
